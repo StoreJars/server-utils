ac04ae31f673a200707a5bad276a114d30437af3
Victor Akerele <eneroakerele@gmail.com>
2020-03-18 20:16:58 +0100

    added winston type

--- src/logger.ts
+++ src/logger.ts
@@ -1,28 +1,30 @@
 import path from 'path';
 import { createLogger, format, transports } from 'winston';
 
-const { colorize, combine, timestamp, label, printf } = format;
+export default function (name: string) {
+  const { colorize, combine, timestamp, label, printf } = format;
 
-const LOGS_DIR = 'logs';
+  const LOGS_DIR = 'logs';
 
-const logFormat = printf(info => {
-  return `${info.timestamp} [ ${info.label} ] ${info.level} : ${info.message}`;
-});
+  const logFormat = printf(info => {
+    return `${info.timestamp} [ ${info.label} ] ${info.level} : ${info.message}`;
+  });
 
-const Log = createLogger({
-  format: combine(label({ label: 'SHOPPIE-USER' }), timestamp(), logFormat),
-  transports: [
-    new transports.File({ filename: path.join(LOGS_DIR, 'error.log'), level: 'error' }),
-    new transports.File({ filename: path.join(LOGS_DIR, 'debug.log'), level: 'info' }),
-  ],
-});
+  const Log = createLogger({
+    format: combine(label({ label: name }), timestamp(), logFormat),
+    transports: [
+      new transports.File({ filename: path.join(LOGS_DIR, 'error.log'), level: 'error' }),
+      new transports.File({ filename: path.join(LOGS_DIR, 'debug.log'), level: 'info' }),
+    ],
+  });
 
-if (process.env.NODE_ENV !== 'production') {
-  Log.add(
-    new transports.Console({
-      format: combine(colorize(), label({ label: 'SHOPPIE-USER' }), timestamp()),
-    }),
-  );
-}
+  if (process.env.NODE_ENV !== 'production') {
+    Log.add(
+      new transports.Console({
+        format: combine(colorize(), label({ label: name }), timestamp()),
+      }),
+    );
+  }
 
-export default Log;
+  return Log;
+};
