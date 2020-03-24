79018ab98854a6a8fd1143f404c033c88eeb4641
Victor Akerele <eneroakerele@gmail.com>
2020-03-24 18:07:56 +0100

    more verbose logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,30 +1,74 @@
 import path from 'path';
 import { createLogger, format, transports } from 'winston';
+import chalk from 'chalk';
+
+const { combine, colorize, label, printf, splat, timestamp } = format;
+
+const LOGS_DIR = 'logs';
+
+const myFormat = printf(
+  info =>
+    `${info.timestamp} ${chalk.cyan(info.label)} ${info.level}: ${info.message}`
+);
+
+function logFormat(loggerLabel) {
+  return (
+    combine(
+      timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+      splat(),
+      colorize(),
+      label({ label: loggerLabel }),
+      myFormat,
+    )
+  )
+}
+
+function createLoggerWithLabel(labelName: string) {
+  return (
+    createLogger({
+      level: process.env.LOG_LEVEL || 'info',
+      transports: [
+        new transports.Console({
+          // timestamp: myFormat,
+          level: 'debug',
+          // showLevel: true,
+          handleExceptions: true,
+          // humanReadableUnhandledException: true,
+        }),
+        new transports.File({
+          filename: path.join(LOGS_DIR, 'error.log'),
+          level: 'error',
+        }),
+        new transports.File({
+          filename: path.join(LOGS_DIR, 'debug.log'),
+          level: 'info',
+        }),
+      ],
+      format: logFormat(labelName),
+    })
+  )
+}
+
 
 export default function (name: string) {
-  const { colorize, combine, timestamp, label, printf } = format;
-
-  const LOGS_DIR = 'logs';
-
-  const logFormat = printf(info => {
-    return `${info.timestamp} [ ${info.label} ] ${info.level} : ${info.message}`;
-  });
-
-  const Log = createLogger({
-    format: combine(label({ label: name }), timestamp(), logFormat),
-    transports: [
-      new transports.File({ filename: path.join(LOGS_DIR, 'error.log'), level: 'error' }),
-      new transports.File({ filename: path.join(LOGS_DIR, 'debug.log'), level: 'info' }),
-    ],
-  });
-
   if (process.env.NODE_ENV !== 'production') {
-    Log.add(
+    createLoggerWithLabel(name).add(
       new transports.Console({
-        format: combine(colorize(), label({ label: name }), timestamp()),
-      }),
+        format: combine(
+          colorize(),
+          label({ label: name }),
+          timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        )
+      })
     );
   }
 
-  return Log;
+  return {
+    gateway: createLoggerWithLabel(`[${name}:gateway]`),
+    config: createLoggerWithLabel(`[${name}:config]`),
+    db: createLoggerWithLabel(`[${name}:db]`),
+    admin: createLoggerWithLabel(`[${name}:admin]`),
+    email: createLoggerWithLabel(`[${name}:email]`),
+    createLoggerWithLabel,
+  }
 };
