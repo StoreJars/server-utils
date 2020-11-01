c05e2eee818d765a6a86d1b982017976f7050db8
Victor Akerele <eneroakerele@gmail.com>
2020-11-01 22:43:06 +0100

    proper update and deactive types

--- src/interfaces/IWrite.ts
+++ src/interfaces/IWrite.ts
@@ -1,6 +1,8 @@
+import { ObjectID } from 'mongodb';
+
 export default interface IWrite<T> {
   create(item: T): Promise<T>;
   createMany(item: T[]): Promise<T>;
-  deactivate(item: Record<string, unknown>): Promise<unknown>;
-  update(documentId: Record<string, unknown>, data: Record<string, unknown>): Promise<unknown>;
+  deactivate(item: ObjectID): Promise<unknown>;
+  update(documentId: ObjectID, data: Record<string, unknown>): Promise<unknown>;
 }
